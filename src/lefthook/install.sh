d6f949bb509b366562fad8a76c173698bacd8d74
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2025-09-18 12:55:49 +0900

    postCreateCommandでlefthook installを実行

--- src/lefthook/install.sh
+++ src/lefthook/install.sh
@@ -4,7 +4,6 @@ set -e
 echo "Installing lefthook..."
 
 VERSION=${VERSION:-"latest"}
-INSTALL_DIR="/usr/local"
 
 DISTRO=""
 if [ -f /etc/alpine-release ]; then
@@ -32,7 +31,7 @@ case "$ARCH" in
     ;;
 esac
 
-# Install required packages on Debian/Ubuntu
+# 依存・推奨パッケージのインストール
 if [ "${DISTRO}" = "alpine" ]; then
   echo "Unsupported distribution"
   exit 1
@@ -55,24 +54,45 @@ else
   exit 1
 fi
 
-# If already installed, skip
 if command -v lefthook >/dev/null 2>&1; then
+  # lefthook がすでにインストールされている場合は何もしない
   echo "lefthook is already installed. Skipping feature install."
-  exit 0
+
+else
+  # latest バージョンを取得
+  if [ "${VERSION}" = "latest" ]; then
+    VERSION=$(
+      curl -fsSL https://api.github.com/repos/evilmartians/lefthook/releases/latest \
+      | grep '"tag_name":' \
+      | sed -E 's/.*"([^\"]+)".*/\1/'
+    )
+  fi
+
+  # lefthook をインストール
+  echo download lefthook: "https://github.com/evilmartians/lefthook/releases/download/${VERSION}/lefthook_${VERSION#v}_Linux_$(uname -m).gz"
+  curl -fsSL "https://github.com/evilmartians/lefthook/releases/download/${VERSION}/lefthook_${VERSION#v}_Linux_$(uname -m).gz" \
+    | gunzip > /usr/local/bin/lefthook
+  chmod +x /usr/local/bin/lefthook
+
 fi
 
-# Determine latest version if requested
-if [ "${VERSION}" = "latest" ]; then
-  VERSION=$(
-    curl -fsSL https://api.github.com/repos/evilmartians/lefthook/releases/latest \
-    | grep '"tag_name":' \
-    | sed -E 's/.*"([^\"]+)".*/\1/'
-  )
+# postCreateCommand 用のスクリプトを作成
+# .git と .lefthook.yml が存在するなら、コンテナの作成後に lefthook install を実行する。
+mkdir -p /usr/local/lefthook
+cat <<EOF >/usr/local/lefthook/setup.sh
+#!/bin/sh
+set -e
+if [ ! -d .git ]; then
+  exit
 fi
 
-# Download and install lefthook
-curl -fsSL "https://github.com/evilmartians/lefthook/releases/download/${VERSION}/lefthook_${VERSION#v}_Linux_$(uname -m).gz" \
-  | gunzip > "${INSTALL_DIR}/bin/lefthook"
-chmod +x "${INSTALL_DIR}/bin/lefthook"
+for file in lefthook.yml .lefthook.yml .config/lefthook.yml lefthook.yaml .lefthook.yaml .config/lefthook.yaml lefthook.toml .lefthook.toml .config/lefthook.toml lefthook.json .lefthook.json .config/lefthook.json; do
+  if [ -f "\${file}" ]; then
+    lefthook install
+    break
+  fi
+done
+EOF
+chmod +x /usr/local/lefthook/setup.sh
 
 echo "lefthook installed successfully."
