29f63cc4b1b933d7fd30db130de36a05c80b66d9
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2025-09-30 20:03:19 +0900

    update codex-cli

--- src/codex-cli/install.sh
+++ src/codex-cli/install.sh
@@ -1,80 +1,150 @@
 #!/bin/sh
 set -e
 
-echo "Installing Codex CLI..."
-
+# devcontainer feature の options
 VERSION=${VERSION:-"latest"}
 OPENAI_API_KEY=${OPENAI_API_KEY:-""}
 
-DISTRO=""
+# 関数定義
+
+# alpine 向けのパッケージインストール関数
+apk_install() {
+  _apk_install_package_list=""
+  for _apk_install_package in "${@}"; do
+    if ! apk info -e "${_apk_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apk_install_package_list="${_apk_install_package_list} ${_apk_install_package}"
+    fi
+  done
+
+  if [ -n "${_apk_install_package_list}" ]; then
+    apk update
+    apk add --no-cache ${_apk_install_package_list}
+  fi
+}
+
+# debian, ubuntu 向けのパッケージインストール関数
+apt_install() {
+  _apt_install_package_list=""
+  for _apt_install_package in "${@}"; do
+    if ! dpkg -l "${_apt_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apt_install_package_list="${_apt_install_package_list} ${_apt_install_package}"
+    fi
+  done
+
+  if [ -n "${_apt_install_package_list}" ]; then
+    apt-get update
+    apt-get install -y --no-install-recommends ${_apt_install_package_list}
+  fi
+}
+
+# curl もしくは wget でインターネットからリソースをダウンロードする関数
+download() {
+    _download_url="${1}"
+
+    if command -v curl >/dev/null 2>&1; then
+        curl -fsSL "${_download_url}"
+
+    elif command -v wget >/dev/null 2>&1; then
+        wget -qO- "${_download_url}"
+
+    else
+        return 1
+    fi
+}
+
+# インストールを開始
+echo "Installing Codex CLI..."
+
+# ディストリビューションの特定
+distro=""
 if [ -f  /etc/alpine-release ]; then
-  DISTRO="alpine"
+  distro="alpine"
 elif [ -f /etc/debian_version ]; then
-  DISTRO="debian"
+  distro="debian"
 elif [ -f /etc/os-release ]; then
-  DISTRO=$(. /etc/os-release && echo "${ID}")
+  distro=$(. /etc/os-release && echo "${ID}")
 fi
 
-ARCH=$(uname -m)
-case "$ARCH" in
+# アーキテクチャの特定
+arch=$(uname -m)
+case "${arch}" in
   x86_64 | amd64)
-    ARCH="x86_64"
+    arch="x86_64"
     ;;
   aarch64 | arm64)
-    ARCH="aarch64"
+    arch="aarch64"
     ;;
   *)
-    echo "Unsupported architecture: ${ARCH}"
+    echo "Unsupported architecture: ${arch}"
     exit 1
     ;;
 esac
 
-# 依存・推奨パッケージのインストール
-if [ "${DISTRO}" = "alpine" ]; then
-  echo "Unsupported distribution"
-  exit 1
-
-elif [ "${DISTRO}" = "debian" ] || [ "${DISTRO}" = "ubuntu" ]; then
-  recommends="coreutils fd-find findutils gawk grep ripgrep sed" # Codex のホワイトリストコマンドを提供するパッケージ
-  require="ca-certificates curl tar" # インストールに必要なパッケージ
-  installed=$(dpkg -l | awk '/^ii/ { print $2 }')
-  missing=""
-  for package in ${require} ${recommends}; do
-    echo "${installed}" | grep -q "^${package}\$" || missing="$missing ${package}"
-  done
-
-  if [ -n "${missing}" ]; then
-    apt-get update
-    apt-get install -y --no-install-recommends ${missing}
-  fi
-
-else
-  echo "Unsupported distribution"
-  exit 1
-fi
-
 if command -v codex >/dev/null 2>&1; then
-  # Codex CLI がすでにインストールされている場合は何もしない
+  # Codex CLI がすでにインストールされている場合、Codex CLI のインストールはしない
   echo "Codex CLI is already installed. Skipping feature install."
 
+  # Codex CLI が利用する推奨パッケージのインストール
+  case "${distro}" in
+    alpine)
+      apk_install coreutils fd findutils gawk grep ripgrep sed
+      ;;
+
+    debian | ubuntu)
+      apt_install coreutils fd-find findutils gawk grep ripgrep sed
+      ;;
+
+    *)
+      echo "Unsupported distribution"
+      exit 1
+      ;;
+  esac
+
 else
+  # install.sh の実行に必要なパッケージと Codex CLI が利用する推奨パッケージのインストール
+  case "${distro}" in
+    alpine)
+      packages="ca-certificates tar"
+      if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+        packages="curl ${packages}"
+      fi
+      packages="coreutils fd findutils gawk grep ripgrep sed ${packages}"
+      apk_install ${packages}
+      ;;
+
+    debian | ubuntu)
+      packages="ca-certificates tar"
+      if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+        packages="curl ${packages}"
+      fi
+      packages="coreutils fd-find findutils gawk grep ripgrep sed ${packages}"
+      apt_install ${packages}
+      ;;
+
+    *)
+      echo "Unsupported distribution"
+      exit 1
+      ;;
+  esac
+
   # latest バージョンを取得
   if [ "${VERSION}" = "latest" ]; then
     VERSION=$(
-      curl -fsSL https://api.github.com/repos/openai/codex/releases/latest \
+      download https://api.github.com/repos/openai/codex/releases/latest \
       | grep '"tag_name":' \
       | sed -E 's/.*"([^"]+)".*/\1/'
     )
   fi
 
   # Codex CLIをインストール
-  echo download Codex CLI: "https://github.com/openai/codex/releases/download/${VERSION}/codex-${ARCH}-unknown-linux-musl.tar.gz"
+  download_url="https://github.com/openai/codex/releases/download/${VERSION}/codex-${arch}-unknown-linux-musl.tar.gz"
+  echo download Codex CLI: "${download_url}"
   mkdir -p /usr/local/codex-cli
-  curl -fsSL "https://github.com/openai/codex/releases/download/${VERSION}/codex-${ARCH}-unknown-linux-musl.tar.gz" \
-    | tar -xz -C /usr/local/codex-cli
+  download "${download_url}" | tar -xz -C /usr/local/codex-cli
   mkdir -p /usr/local/bin
   ln -s /usr/local/codex-cli/codex-x86_64-unknown-linux-musl /usr/local/bin/codex
 
+  echo "Codex CLI installed successfully."
 fi
 
 # postCreateCommand 用のスクリプトを作成
@@ -91,5 +161,3 @@ mkdir -p ~/.codex
 codex login --api-key "${OPENAI_API_KEY}"
 EOF
 chmod +x /usr/local/codex-cli/setup.sh
-
-echo "Codex CLI installed successfully."
