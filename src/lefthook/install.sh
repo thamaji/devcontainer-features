c2a3a2759f1394666e2333d55054dbc8733ff60f
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2025-09-30 20:04:13 +0900

    update lefthook

--- src/lefthook/install.sh
+++ src/lefthook/install.sh
@@ -1,79 +1,130 @@
 #!/bin/sh
 set -e
 
-echo "Installing lefthook..."
-
+# devcontainer feature の options
 VERSION=${VERSION:-"latest"}
 
-DISTRO=""
-if [ -f /etc/alpine-release ]; then
-  DISTRO="alpine"
+# 関数定義
+
+# alpine 向けのパッケージインストール関数
+apk_install() {
+  _apk_install_package_list=""
+  for _apk_install_package in "${@}"; do
+    if ! apk info -e "${_apk_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apk_install_package_list="${_apk_install_package_list} ${_apk_install_package}"
+    fi
+  done
+
+  if [ -n "${_apk_install_package_list}" ]; then
+    apk update
+    apk add --no-cache ${_apk_install_package_list}
+  fi
+}
+
+# debian, ubuntu 向けのパッケージインストール関数
+apt_install() {
+  _apt_install_package_list=""
+  for _apt_install_package in "${@}"; do
+    if ! dpkg -l "${_apt_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apt_install_package_list="${_apt_install_package_list} ${_apt_install_package}"
+    fi
+  done
+
+  if [ -n "${_apt_install_package_list}" ]; then
+    apt-get update
+    apt-get install -y --no-install-recommends ${_apt_install_package_list}
+  fi
+}
+
+# curl もしくは wget でインターネットからリソースをダウンロードする関数
+download() {
+    _download_url="${1}"
+
+    if command -v curl >/dev/null 2>&1; then
+        curl -fsSL "${_download_url}"
+
+    elif command -v wget >/dev/null 2>&1; then
+        wget -qO- "${_download_url}"
+
+    else
+        return 1
+    fi
+}
+
+# インストールを開始
+echo "Installing lefthook..."
+
+# ディストリビューションの特定
+distro=""
+if [ -f  /etc/alpine-release ]; then
+  distro="alpine"
 elif [ -f /etc/debian_version ]; then
-  DISTRO="debian"
+  distro="debian"
 elif [ -f /etc/os-release ]; then
-  DISTRO=$(. /etc/os-release && echo "${ID}")
+  distro=$(. /etc/os-release && echo "${ID}")
 fi
 
-ARCH=$(uname -m)
-case "$ARCH" in
+# アーキテクチャの特定
+arch=$(uname -m)
+case "${arch}" in
   x86_64 | amd64)
-    ARCH="x86_64"
+    arch="x86_64"
     ;;
-  arm64)
-    ARCH="arm64"
-    ;;
-  aarch64)
-    ARCH="aarch64"
+  aarch64 | arm64)
+    arch="arm64"
     ;;
   *)
-    echo "Unsupported architecture: ${ARCH}"
+    echo "Unsupported architecture: ${arch}"
     exit 1
     ;;
 esac
-
-# 依存・推奨パッケージのインストール
-if [ "${DISTRO}" = "alpine" ]; then
-  echo "Unsupported distribution"
-  exit 1
-
-elif [ "${DISTRO}" = "debian" ] || [ "${DISTRO}" = "ubuntu" ]; then
-  require="ca-certificates curl gzip"
-  installed=$(dpkg -l | awk '/^ii/ { print $2 }')
-  missing=""
-  for package in ${require}; do
-    echo "${installed}" | grep -q "^${package}$" || missing="$missing ${package}"
-  done
-
-  if [ -n "${missing}" ]; then
-    apt-get update
-    apt-get install -y --no-install-recommends ${missing}
-  fi
-
-else
-  echo "Unsupported distribution"
-  exit 1
-fi
 
 if command -v lefthook >/dev/null 2>&1; then
   # lefthook がすでにインストールされている場合は何もしない
   echo "lefthook is already installed. Skipping feature install."
 
 else
+  # install.sh の実行に必要なパッケージのインストール
+  case "${distro}" in
+    alpine)
+      packages="ca-certificates gzip"
+      if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+        packages="curl ${packages}"
+      fi
+      apk_install ${packages}
+      ;;
+
+    debian | ubuntu)
+      packages="ca-certificates gzip"
+      if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+        packages="curl ${packages}"
+      fi
+      apt_install ${packages}
+      ;;
+
+    *)
+      echo "Unsupported distribution"
+      exit 1
+      ;;
+  esac
+
   # latest バージョンを取得
   if [ "${VERSION}" = "latest" ]; then
     VERSION=$(
-      curl -fsSL https://api.github.com/repos/evilmartians/lefthook/releases/latest \
+      download https://api.github.com/repos/evilmartians/lefthook/releases/latest \
       | grep '"tag_name":' \
       | sed -E 's/.*"([^\"]+)".*/\1/'
     )
   fi
 
   # lefthook をインストール
-  echo download lefthook: "https://github.com/evilmartians/lefthook/releases/download/${VERSION}/lefthook_${VERSION#v}_Linux_$(uname -m).gz"
-  curl -fsSL "https://github.com/evilmartians/lefthook/releases/download/${VERSION}/lefthook_${VERSION#v}_Linux_$(uname -m).gz" \
-    | gunzip > /usr/local/bin/lefthook
+  download_url="https://github.com/evilmartians/lefthook/releases/download/${VERSION}/lefthook_${VERSION#v}_Linux_${arch}.gz"
+  echo download lefthook: "${download_url}"
+  mkdir -p /usr/local/bin
+  download "${download_url}" | gunzip > /usr/local/bin/lefthook
   chmod +x /usr/local/bin/lefthook
 
+  echo "lefthook installed successfully."
 fi
 
 # postCreateCommand 用のスクリプトを作成
@@ -94,5 +145,3 @@ for file in lefthook.yml .lefthook.yml .config/lefthook.yml lefthook.yaml .lefth
 done
 EOF
 chmod +x /usr/local/lefthook/setup.sh
-
-echo "lefthook installed successfully."
