c180f95875297613fd204c0a0361a0bceb490f25
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2025-09-30 20:04:00 +0900

    update gitleaks

--- src/gitleaks/install.sh
+++ src/gitleaks/install.sh
@@ -1,74 +1,128 @@
 #!/bin/sh
 set -e
 
-echo "Installing gitleaks..."
-
+# devcontainer feature の options
 VERSION=${VERSION:-"latest"}
 
-DISTRO=""
-if [ -f /etc/alpine-release ]; then
-  DISTRO="alpine"
-elif [ -f /etc/debian_version ]; then
-  DISTRO="debian"
-elif [ -f /etc/os-release ]; then
-  DISTRO=$(. /etc/os-release && echo "${ID}")
-fi
+# 関数定義
 
-ARCH=$(uname -m)
-case "$ARCH" in
-  x86_64 | amd64)
-    ARCH="x64"
-    ;;
-  aarch64 | arm64)
-    ARCH="arm64"
-    ;;
-  *)
-    echo "Unsupported architecture: ${ARCH}"
-    exit 1
-    ;;
-esac
-
-# 依存・推奨パッケージのインストール
-if [ "${DISTRO}" = "alpine" ]; then
-  echo "Unsupported distribution"
-  exit 1
-
-elif [ "${DISTRO}" = "debian" ] || [ "${DISTRO}" = "ubuntu" ]; then
-  require="ca-certificates curl gzip tar"
-  installed=$(dpkg -l | awk '/^ii/ { print $2 }')
-  missing=""
-  for package in ${require}; do
-    echo "${installed}" | grep -q "^${package}$" || missing="$missing ${package}"
+# alpine 向けのパッケージインストール関数
+apk_install() {
+  _apk_install_package_list=""
+  for _apk_install_package in "${@}"; do
+    if ! apk info -e "${_apk_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apk_install_package_list="${_apk_install_package_list} ${_apk_install_package}"
+    fi
   done
 
-  if [ -n "${missing}" ]; then
-    apt-get update
-    apt-get install -y --no-install-recommends ${missing}
+  if [ -n "${_apk_install_package_list}" ]; then
+    apk update
+    apk add --no-cache ${_apk_install_package_list}
   fi
+}
 
-else
-  echo "Unsupported distribution"
-  exit 1
-fi
+# debian, ubuntu 向けのパッケージインストール関数
+apt_install() {
+  _apt_install_package_list=""
+  for _apt_install_package in "${@}"; do
+    if ! dpkg -l "${_apt_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apt_install_package_list="${_apt_install_package_list} ${_apt_install_package}"
+    fi
+  done
 
-  # gitleaks がすでにインストールされている場合は何もしない
+  if [ -n "${_apt_install_package_list}" ]; then
+    apt-get update
+    apt-get install -y --no-install-recommends ${_apt_install_package_list}
+  fi
+}
+
+# curl もしくは wget でインターネットからリソースをダウンロードする関数
+download() {
+    _download_url="${1}"
+
+    if command -v curl >/dev/null 2>&1; then
+        curl -fsSL "${_download_url}"
+
+    elif command -v wget >/dev/null 2>&1; then
+        wget -qO- "${_download_url}"
+
+    else
+        return 1
+    fi
+}
+
+# インストールを開始
+echo "Installing gitleaks..."
+
+# gitleaks がすでにインストールされている場合は何もしない
 if command -v gitleaks >/dev/null 2>&1; then
   echo "gitleaks is already installed. Skipping feature install."
   exit 0
 fi
 
+# ディストリビューションの特定
+distro=""
+if [ -f  /etc/alpine-release ]; then
+  distro="alpine"
+elif [ -f /etc/debian_version ]; then
+  distro="debian"
+elif [ -f /etc/os-release ]; then
+  distro=$(. /etc/os-release && echo "${ID}")
+fi
+
+# アーキテクチャの特定
+arch=$(uname -m)
+case "${arch}" in
+  x86_64 | amd64)
+    arch="x64"
+    ;;
+  aarch64 | arm64)
+    arch="arm64"
+    ;;
+  *)
+    echo "Unsupported architecture: ${arch}"
+    exit 1
+    ;;
+esac
+
+
+# install.sh の実行に必要なパッケージのインストール
+case "${distro}" in
+  alpine)
+    packages="ca-certificates gzip tar"
+    if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+      packages="curl ${packages}"
+    fi
+    apk_install ${packages}
+    ;;
+
+  debian | ubuntu)
+    packages="ca-certificates gzip tar"
+    if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+      packages="curl ${packages}"
+    fi
+    apt_install ${packages}
+    ;;
+
+  *)
+    echo "Unsupported distribution"
+    exit 1
+    ;;
+esac
+
 # latest バージョンを取得
 if [ "${VERSION}" = "latest" ]; then
   VERSION=$(
-    curl -fsSL https://api.github.com/repos/zricethezav/gitleaks/releases/latest \
+    download https://api.github.com/repos/zricethezav/gitleaks/releases/latest \
     | grep '"tag_name":' \
     | sed -E 's/.*"([^\"]+)".*/\1/'
   )
 fi
 
 # gitleaks をインストール
-echo download gitleaks: "https://github.com/zricethezav/gitleaks/releases/download/${VERSION}/gitleaks_${VERSION#v}_linux_${ARCH}.tar.gz"
-curl -fsSL "https://github.com/zricethezav/gitleaks/releases/download/${VERSION}/gitleaks_${VERSION#v}_linux_${ARCH}.tar.gz" \
-  | tar -xz -C /usr/local/bin gitleaks
+download_url="https://github.com/zricethezav/gitleaks/releases/download/${VERSION}/gitleaks_${VERSION#v}_linux_${arch}.tar.gz"
+echo download gitleaks: "${download_url}"
+mkdir -p /usr/local/bin
+download "${download_url}" | tar -xz -C /usr/local/bin gitleaks
 
 echo "gitleaks installed successfully."
