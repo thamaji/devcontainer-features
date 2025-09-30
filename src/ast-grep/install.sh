0cea16be0e2a748119e890ee95fa1ed81b2280d0
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2025-09-30 20:03:08 +0900

    update ast-grep

--- src/ast-grep/install.sh
+++ src/ast-grep/install.sh
@@ -1,80 +1,131 @@
 #!/bin/sh
-set -e
+set -eu
 
-echo "Installing ast-grep(sg)..."
-
+# devcontainer feature の options
 VERSION=${VERSION:-"latest"}
-OPENAI_API_KEY=${OPENAI_API_KEY:-""}
 
-DISTRO=""
-if [ -f  /etc/alpine-release ]; then
-  DISTRO="alpine"
-elif [ -f /etc/debian_version ]; then
-  DISTRO="debian"
-elif [ -f /etc/os-release ]; then
-  DISTRO=$(. /etc/os-release && echo "${ID}")
-fi
+# 関数定義
 
-ARCH=$(uname -m)
-case "$ARCH" in
-  x86_64 | amd64)
-    ARCH="x86_64"
-    ;;
-  aarch64 | arm64)
-    ARCH="aarch64"
-    ;;
-  *)
-    echo "Unsupported architecture: ${ARCH}"
-    exit 1
-    ;;
-esac
-
-# 依存・推奨パッケージのインストール
-if [ "${DISTRO}" = "alpine" ]; then
-  echo "Unsupported distribution"
-  exit 1
-
-elif [ "${DISTRO}" = "debian" ] || [ "${DISTRO}" = "ubuntu" ]; then
-  require="ca-certificates curl unzip"
-  installed=$(dpkg -l | awk '/^ii/ { print $2 }')
-  missing=""
-  for package in ${require}; do
-    echo "${installed}" | grep -q "^${package}$" || missing="$missing ${package}"
+# alpine 向けのパッケージインストール関数
+apk_install() {
+  _apk_install_package_list=""
+  for _apk_install_package in "${@}"; do
+    if ! apk info -e "${_apk_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apk_install_package_list="${_apk_install_package_list} ${_apk_install_package}"
+    fi
   done
 
-  if [ -n "${missing}" ]; then
-    apt-get update
-    apt-get install -y --no-install-recommends ${missing}
+  if [ -n "${_apk_install_package_list}" ]; then
+    apk update
+    apk add --no-cache ${_apk_install_package_list}
   fi
+}
 
-else
-  echo "Unsupported distribution"
-  exit 1
-fi
+# debian, ubuntu 向けのパッケージインストール関数
+apt_install() {
+  _apt_install_package_list=""
+  for _apt_install_package in "${@}"; do
+    if ! dpkg -l "${_apt_install_package}" > /dev/null 2>&1 || [ "${_apt_install_package}" = "ca-certificates" ]; then
+      _apt_install_package_list="${_apt_install_package_list} ${_apt_install_package}"
+    fi
+  done
 
+  if [ -n "${_apt_install_package_list}" ]; then
+    apt-get update
+    apt-get install -y --no-install-recommends ${_apt_install_package_list}
+  fi
+}
+
+# curl もしくは wget でインターネットからリソースをダウンロードする関数
+download() {
+    _download_url="${1}"
+
+    if command -v curl >/dev/null 2>&1; then
+        curl -fsSL "${_download_url}"
+
+    elif command -v wget >/dev/null 2>&1; then
+        wget -qO- "${_download_url}"
+
+    else
+        return 1
+    fi
+}
+
+# インストールを開始
+echo "Installing ast-grep(sg)..."
+
+# ast-grep(sg) がすでにインストールされている場合は何もしない
 if command -v ast-grep >/dev/null 2>&1; then
-  # ast-grep(sg) がすでにインストールされている場合は何もしない
   echo "ast-grep(sg) is already installed. Skipping feature install."
   exit 0
 fi
 
+# ディストリビューションの特定
+distro=""
+if [ -f  /etc/alpine-release ]; then
+  distro="alpine"
+elif [ -f /etc/debian_version ]; then
+  distro="debian"
+elif [ -f /etc/os-release ]; then
+  distro=$(. /etc/os-release && echo "${ID}")
+fi
+
+# アーキテクチャの特定
+arch=$(uname -m)
+case "${arch}" in
+  x86_64 | amd64)
+    arch="x86_64"
+    ;;
+  aarch64 | arm64)
+    arch="aarch64"
+    ;;
+  *)
+    echo "Unsupported architecture: ${arch}"
+    exit 1
+    ;;
+esac
+
+# install.sh の実行に必要なパッケージのインストール
+case "${distro}" in
+  alpine)
+    packages="ca-certificates unzip"
+    if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+      packages="curl ${packages}"
+    fi
+    apk_install ${packages}
+    ;;
+
+  debian | ubuntu)
+    packages="ca-certificates unzip"
+    if ! command -v wget >/dev/null 2>&1 && ! command -v curl >/dev/null 2>&1; then
+      packages="curl ${packages}"
+    fi
+    apt_install ${packages}
+    ;;
+
+  *)
+    echo "Unsupported distribution"
+    exit 1
+    ;;
+esac
 
 # latest バージョンを取得
 if [ "${VERSION}" = "latest" ]; then
   VERSION=$(
-    curl -fsSL https://api.github.com/repos/ast-grep/ast-grep/releases/latest \
+    download https://api.github.com/repos/ast-grep/ast-grep/releases/latest \
     | grep '"tag_name":' \
     | sed -E 's/.*"([^"]+)".*/\1/'
   )
 fi
 
 # ast-grep(sg) をインストール
-echo download Codex CLI: "https://github.com/ast-grep/ast-grep/releases/download/${VERSION}/app-${ARCH}-unknown-linux-gnu.zip"
+download_url="https://github.com/ast-grep/ast-grep/releases/download/${VERSION}/app-${arch}-unknown-linux-gnu.zip"
 install_dir="/usr/local/ast-grep-${VERSION}"
+echo download Codex CLI: "${download_url}"
 mkdir -p "${install_dir}"
-curl -fsSL -o "${install_dir}/app-${ARCH}-unknown-linux-gnu.zip" "https://github.com/ast-grep/ast-grep/releases/download/${VERSION}/app-${ARCH}-unknown-linux-gnu.zip"
-unzip "${install_dir}/app-${ARCH}-unknown-linux-gnu.zip" -d "${install_dir}"
-rm "${install_dir}/app-${ARCH}-unknown-linux-gnu.zip"
+download "${download_url}" > "${install_dir}/app-${arch}-unknown-linux-gnu.zip"
+unzip "${install_dir}/app-${arch}-unknown-linux-gnu.zip" -d "${install_dir}"
+rm "${install_dir}/app-${arch}-unknown-linux-gnu.zip"
 ln -s "${install_dir}/ast-grep" /usr/local/bin/ast-grep
 ln -s "${install_dir}/sg" /usr/local/bin/sg
 
